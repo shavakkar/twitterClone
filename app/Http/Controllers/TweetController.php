3b3186d1d70010565419ab2ac70970648d365697
shavakkar <shavakkar@gmail.com>
2024-02-14 22:32:03 +0530

    controllers set

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -31,7 +31,36 @@ class TweetController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $file = null;
+        $extension = null;
+        $filename = null;
+        $path = '';
+
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $request->validate(['file' => 'required|mimes:jpg,png,mp4']);
+            $extension = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $extension;
+            $extension === 'mp4' ? $path = '/video/' : $path = '/pic/';
+        }
+
+        $tweet = new Tweet;
+
+        $tweet->name = 'Test';
+        $tweet->handle = '@test';
+        $tweet->image = 'https://yt3.ggpht.com/e9o-24_frmNSSVvjS47rT8qCHgsHNiedqgXbzmrmpsj6H1ketcufR1B9vLXTZRa30krRksPj=s176-c-k-c0x00ffffff-no-rj-mo';
+        $tweet->tweet = $request->input('tweet');
+        if ($filename) {
+            $tweet->file = $path . $filename;
+            $tweet->is_video = $extension === 'mp4' ? true : false;
+            $file->move(public_path() . $path, $filename);
+        }
+        $tweet->comments = rand(5, 500);
+        $tweet->retweets = rand(5, 500);
+        $tweet->likes = rand(5, 500);
+        $tweet->analytics = rand(5, 500);
+
+        $tweet->save();
     }
 
     /**
@@ -61,8 +90,16 @@ class TweetController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tweet $tweet)
+    public function destroy($id)
     {
-        //
+        $tweet = Tweet::find($id);
+
+        if (!is_null($tweet->file) && file_exists(public_path() . $tweet->file)) {
+            unlink(public_path() . $tweet->file);
+        }
+
+        $tweet->delete();
+
+        return redirect()->route('tweets.index');
     }
 }
