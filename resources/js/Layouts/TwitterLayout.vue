40b75a8ffe83a661ba3a228f62fbd4cb475ec005
shavakkar <shavakkar@gmail.com>
2024-02-14 21:41:25 +0530

    frontend almost done

--- resources/js/Layouts/TwitterLayout.vue
+++ resources/js/Layouts/TwitterLayout.vue
@@ -103,12 +103,14 @@ const textareaInput = (e) => {
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="absolute top-0 z-0 h-full overflow-auto scrollbar-hide">
-                <div class="mt-[126px]"></div>
-                <slot />
-                <div class="pb-4"></div>
+                <div
+                    class="absolute top-0 z-0 h-full overflow-auto scrollbar-hide"
+                >
+                    <div class="mt-[126px]"></div>
+                    <slot />
+                    <div class="pb-4"></div>
+                </div>
             </div>
 
             <div
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,12 +2,32 @@
 import { Head, Link } from "@inertiajs/vue3";
 
 import TwitterLayout from "@/Layouts/TwitterLayout.vue";
+
+import Tweet from "@/Components/Tweet.vue";
 </script>
 
 <template>
     <Head title="Welcome" />
 
     <TwitterLayout>
-        <!-- <div class="bg-red-600">Welcome</div> -->
+        <div class="text-white">
+            <div class="flex">
+                <Tweet
+                    :tweet="{
+                        name: 'Test',
+                        handle: '@test',
+                        image: 'https://randomuser.me/api/portraits/men/40.jpg',
+                        tweet: 'kjhfjdhf;jhk;jdfhdjjldsgljgjfbjdbfvkdvfjshfkhgljghflehgfjehgfjh',
+                        file: '/video/warrior.mp4',
+                        is_video: true,
+                        comments: '35',
+                        retweets: '34',
+                        likes: '88',
+                        analytics: '81',
+                    }"
+                />
+            </div>
+            <div class="border-b border-b-gray-800 mt-2"></div>
+        </div>
     </TwitterLayout>
 </template>
