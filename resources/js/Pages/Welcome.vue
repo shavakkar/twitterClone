7f25956f84b54dcdcc5ea0c80587e94da34db4b0
shavakkar <shavakkar@gmail.com>
2024-02-14 22:13:54 +0530

    seed, routes done

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,6 +4,8 @@ import { Head, Link } from "@inertiajs/vue3";
 import TwitterLayout from "@/Layouts/TwitterLayout.vue";
 
 import Tweet from "@/Components/Tweet.vue";
+
+defineProps({ tweets: Array });
 </script>
 
 <template>
@@ -11,21 +13,8 @@ import Tweet from "@/Components/Tweet.vue";
 
     <TwitterLayout>
         <div class="text-white">
-            <div class="flex">
-                <Tweet
-                    :tweet="{
-                        name: 'Test',
-                        handle: '@test',
-                        image: 'https://randomuser.me/api/portraits/men/40.jpg',
-                        tweet: 'kjhfjdhf;jhk;jdfhdjjldsgljgjfbjdbfvkdvfjshfkhgljghflehgfjehgfjh',
-                        file: '/video/warrior.mp4',
-                        is_video: true,
-                        comments: '35',
-                        retweets: '34',
-                        likes: '88',
-                        analytics: '81',
-                    }"
-                />
+            <div class="flex" v-for="tweet in tweets" :key="tweet">
+                <Tweet :tweet="tweet" />
             </div>
             <div class="border-b border-b-gray-800 mt-2"></div>
         </div>
